8f62cd9b2244de1969d04b57968f54d44e2c5afb
Nicolas Lesconnec <nicolas.lesconnec@sigfox.com>
2019-03-28 12:05:57 +0100

    Sample Code update
    Courtesy of Jim Sheridan <jim@sheridan.dk>

--- mkrfox-send-internal-temperature.ino
+++ mkrfox-send-internal-temperature.ino
@@ -1,22 +1,60 @@
-// Arduino SigFox for MKRFox1200 - Version: Latest 
+#include <RTCZero.h>
+#include <ArduinoLowPower.h>
 #include <SigFox.h>
 
-/*
-
-*/
-typedef struct __attribute__ ((packed)) sigfox_message{
-  int8_t temp;
-}SigfoxMessage;
+#define LED 6         // This is the built_in led
 
 void setup() {
-    Serial.begin(9600);
-    if (!SigFox.begin()){
-      Serial.println("UNEXPECTED ERROR ... PLEASE WAIT");
-      NVIC_SystemReset();
-      while(1);
-    }
-    SigFox.reset();
-    delay(100);
-    SigFox.debug();
-    SigFox.end();
+   pinMode(LED,OUTPUT);
+   digitalWrite(LED,LOW);
+
+   Serial.begin(9600);
+
+   while (!Serial);
+
+   if ( ! SigFox.begin() ) {
+     Serial.println("Error ... rebooting");
+     NVIC_SystemReset();
+     while(1);
+  }
+  SigFox.reset();
+
+  delay(100);
+
+  SigFox.debug();
+  SigFox.end();
+
+  // We need to have to time to program the Arduino after a reset
+  // Otherwise it does not respond when in low-power mode
+  Serial.println("Booting...");
+  digitalWrite(LED,HIGH);
+
+  delay(5000);
+
+  digitalWrite(LED,LOW);
+}
+
+typedef struct __attribute__ ((packed)) sigfox_message {
+  int8_t temp;
+} SigfoxMessage;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  SigFox.begin();
+  SigFox.status();
+
+  SigfoxMessage msg;
+  msg.temp = (int8_t)SigFox.internalTemperature();
+
+  SigFox.beginPacket();
+  SigFox.write((uint8_t*)&msg,sizeof(msg));
+  SigFox.endPacket(false);
+
+  SigFox.end();
+
+  // Wait for 10 minutes.
+  // Low Power version - be carefull of bug
+  //   LowPower.sleep(10*60*1000);
+  // Normal version
+  delay(10*60*1000);
 }
